d441aeff1229f016f6b532a04a8ce62ed21659b4
1vgagan <97120291+1vgagan@users.noreply.github.com>
2022-05-07 20:46:12 +0530

    Create DataTypes.c

---
+++ DataTypes.c
@@ -0,0 +1,29 @@
+// Online C compiler to run C program online
+#include <stdio.h> 
+
+int main() {   
+
+    int salary = 500000;
+    //Variable = salary
+    //Variale value = 500000
+    //Variable's Value datatype = int(integer)
+    
+    char classSection = 'D';
+    //Varibale = classSection
+    //Variable value = D
+    //Variable's Value datatype = character
+    
+    float temperature = 43.1;
+    //Varibale = temperature
+    //Variable value = 43.1
+    //Variable's Value datatype = float
+    
+    char designation [] = "Manager";
+    //Varibale = designation
+    //Variable value = Manager
+    //Variable's Value datatype = Character Array(which is also called as String)
+    
+    printf("%c",designation[1]);
+    // "%d" = printing interger variable value
+
+} 
